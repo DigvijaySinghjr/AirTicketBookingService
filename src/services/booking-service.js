496b26c6b218cd665c3f2e2560beb3810bf07795
DigvijaySinghjr <neymar9838jr@gmail.com>
2025-07-09 22:51:15 +0530

    just to make it check by Copilot

---
+++ src/services/booking-service.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const { BookingRepository } = require('../repository/index');
+const { FLIGHT_SERVICE_PATH } = require('../config/serverConfig')
+const {ServiceError } = require('../utils/errors')
+const { StatusCodes } = require('http-status-codes');
+
+class BookingService {
+    constructor() {
+        this.bookingRepository = new BookingRepository();
+    }
+
+    async createBooking(data) {
+        try {
+            const flightId = data.flightId;
+            let getFlightRequestURL = `${FLIGHT_SERVICE_PATH}/api/v1/flights/${flightId}`;
+            const flight = await axios.get(getFlightRequestURL);
+            return flight.data;
+        } catch (error) {
+            throw new ServiceError();
+        }
+    }
+
+}
+
+module.exports = BookingService;
