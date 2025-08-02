d5843c7be55854f21d894acb77b67bde7422122d
DigvijaySinghjr <neymar9838jr@gmail.com>
2025-08-02 13:49:32 +0530

    Added rabbit mq config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-const v1ApiRoutes = require('./v1/index');
+const {PORT} = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
 
-const router = express.Router();
+const setupAndStartServer = () => {
 
-router.use('/v1', v1ApiRoutes);
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
 
-module.exports = router;
+    app.use('/api', apiRoutes);
+
+    app.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter: true});
+        }
+    });
+}
+
+setupAndStartServer();
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
-const { BookingController } = require('../../controllers/index');
+const { BookingController }  = require('../../controllers/index');
+//const { createChannel } = require('../../utils/messageQueue')
 
+//const channel =  await createChannel();                           //we cant use async/await here because this file is loaded before the app starts
+const bookingController = new BookingController();
 const router = express.Router();
 
-router.post('/bookings', BookingController.create);
+router.post('/bookings', bookingController.create);
+router.post('/publish', bookingController.sendMesageToQueue);
+
 
 module.exports = router;
